96b5c037a44b40980294946984fdbcf9c137bfe3
Pritika Agarwal <pritikaagarwal0731@gmail.com>
2025-05-02 10:16:12 +0530

    Update events_calendar.cpp

--- events_calendar.cpp
+++ events_calendar.cpp
@@ -43,23 +43,6 @@ string month() {
     }
 }
 
-string occasion()
-{
-    string occ;
-    cout << "Enter the event scheduled for the day: ";
-
-    cin.ignore(); // Prevents input buffer issues
-    getline(cin, occ);
-
-    // Validate non-empty input
-    if (occ.empty()) {
-        cout << "Event description cannot be empty! Please enter again.\n";
-        getline(cin, occ);
-    }
-
-    return occ;
-}
-
 string type()
 {
     char reply;
